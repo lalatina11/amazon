f7a99a380d6d18690463bc0defc9b74c56402854
lalatina11 <rahmadancandra@gmail.com>
2026-05-09 09:31:28 +0700

    feat: add to cart services

--- lib/src/feature/cart/services/cart_services.dart
+++ lib/src/feature/cart/services/cart_services.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:amazon/src/config/env_config.dart';
 import 'package:amazon/src/model/api_response.dart';
 import "package:http/http.dart" as http;
@@ -21,6 +23,27 @@ class CartServices {
       return ApiResponse(
         success: false,
         message: "Failed to get carts data",
+        data: null,
+      );
+    }
+  }
+
+  Future<ApiResponse> addToCart({
+    required String token,
+    required String productId,
+    required int qty,
+  }) async {
+    try {
+      final res = await http.post(
+        envConfig.getApiBaseUrl(path: "/cart/add-to-cart"),
+        headers: {...defaultHeaders, "Authorization": "Bearer $token"},
+        body: jsonEncode({"productId": productId, "qty": qty}),
+      );
+      return ApiResponse.fromJson(res.body);
+    } catch (e) {
+      return ApiResponse(
+        success: false,
+        message: "Gagal menambahkan produk ke keranjang",
         data: null,
       );
     }
