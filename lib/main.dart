52e476b9b0ac5ea9139182b10edd1884424e3618
lalatina11 <rahmadancandra@gmail.com>
2026-05-10 07:46:41 +0700

    optimize: ensure initialized

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load();
   await initializeDateFormatting('id_ID', null); // ← add this
   final SharedPreferences sharedPreferences =
